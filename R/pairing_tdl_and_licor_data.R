9d02445795fe42481279400afb523ddc0644598a
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-08-17 21:55:59 -0500

    Update a few more functions to use `exdf`

--- R/pairing_tdl_and_licor_data.R
+++ R/pairing_tdl_and_licor_data.R
@@ -1,89 +1,61 @@
 # Specify the respiration rate (in units of micromol / m^2 / s) to store in the
 # Licor file. (The rate can be supplied as a positive or negative number, and
 # its absolute value will be stored.)
-specify_respiration <- function(licor_file, respiration) {
-    # Add some new columns to the Licor file in preparation for adding the
+specify_respiration <- function(licor_exdf, respiration) {
+    # Add a new column to the Licor file in preparation for adding the
     # respiration information
-    licor_file <- specify_variables(
-        licor_file,
+    licor_exdf <- specify_variables(
+        licor_exdf,
         c("in", "respiration", "micromol m^(-2) s^(-1)")
     )
 
     # Store it in the Licor file and return the updated file
-    licor_file[['main_data']][['respiration']] <- abs(respiration)
+    licor_exdf[,'respiration'] <- abs(respiration)
 
-    return(licor_file)
+    return(licor_exdf)
 }
 
-batch_specify_respiration <- function(licor_files, respiration) {
-    lapply(
-        licor_files,
-        function(licor_file) {
-            specify_respiration(licor_file, respiration)
-        }
-    )
+batch_specify_respiration <- function(licor_exdfs, respiration) {
+    lapply(licor_exdfs, function(x) {specify_respiration(x, respiration)})
 }
 
 # Specify the oxygen level (as a percentage, typically either 2 or 21) to store
 # in the Licor file
-specify_oxygen <- function(licor_file, oxygen) {
-    # Add some new columns to the Licor file in preparation for adding the
+specify_oxygen <- function(licor_exdf, oxygen) {
+    # Add a new column to the Licor file in preparation for adding the
     # oxygen information
-    licor_file <- specify_variables(
-        licor_file,
+    licor_exdf <- specify_variables(
+        licor_exdf,
         c("in", "Oxygen", "%")
     )
 
     # Store it in the Licor file and return the updated file
-    licor_file[['main_data']][['Oxygen']] <- oxygen
+    licor_exdf[,'Oxygen'] <- oxygen
 
-    return(licor_file)
+    return(licor_exdf)
 }
 
-batch_specify_oxygen <- function(licor_files, oxygen) {
-    lapply(
-        licor_files,
-        function(licor_file) {
-            specify_oxygen(licor_file, oxygen)
-        }
-    )
+batch_specify_oxygen <- function(licor_exdfs, oxygen) {
+    lapply(licor_exdfs, function(x) {specify_oxygen(x, oxygen)})
 }
 
-get_oxygen_info_from_preamble <- function(licor_file) {
-    # Add some new columns to the Licor file in preparation for adding the
+get_oxygen_info_from_preamble <- function(licor_exdf) {
+    # Add a new column to the Licor file in preparation for adding the
     # oxygen information
-    licor_file <- specify_variables(
-        licor_file,
+    licor_exdf <- specify_variables(
+        licor_exdf,
         c("in", "Oxygen", "%")
     )
 
     # Try to get the oxygen information from the Licor file's preamble
     oxygen <- c()
-    preamble <- licor_file[['preamble']]
+    preamble <- licor_exdf[['preamble']]
     for (pe in preamble) {
         if ("Oxygen" %in% colnames(pe)) {
-            # Try to apply as.numeric, but if any warnings or errors occur,
-            # warn the user
-            msg <- paste0(
-                "The 'Oxygen' value specified in the preamble of Licor file\n",
-                licor_file[['file_name']],
-                "\ncould not be converted to a numeric value. Value was: ",
-                pe[['Oxygen']]
-            )
-            tryCatch(
-                {
-                    # Code to be executed initially
-                    oxygen <- as.numeric(pe[['Oxygen']])
-                },
-                error=function(cond) {
-                    # Code for handling errors
-                    stop(msg)
-                },
-                warning=function(cond) {
-                    # Code for handling warnings
-                    stop(msg)
-                }
-            )
+            # Convert the value to numeric form, if possible, and stop going
+            # through the preamble
+            oxygen <- try_as_numeric(pe[['Oxygen']])
+            break
         }
     }
 
@@ -91,7 +63,7 @@ get_oxygen_info_from_preamble <- function(licor_file) {
     if (length(oxygen) == 0) {
         msg <- paste0(
             "Could not automatically get oxygen information from Licor file:\n'",
-            licor_file[['file_name']],
+            licor_exdf[['file_name']],
             "'\nConsider adding it with the `specify_oxygen` function rather ",
             "than using `get_oxygen_info_from_preamble`"
         )
@@ -99,25 +71,20 @@ get_oxygen_info_from_preamble <- function(licor_file) {
     }
 
     # Store it in the Licor file and return the updated file
-    licor_file[['main_data']][['Oxygen']] <- oxygen
+    licor_exdf[,'Oxygen'] <- oxygen
 
-    return(licor_file)
+    return(licor_exdf)
 }
 
-batch_get_oxygen_info_from_preamble <- function(licor_files) {
-    lapply(
-        licor_files,
-        function(licor_file) {
-            get_oxygen_info_from_preamble(licor_file)
-        }
-    )
+batch_get_oxygen_info_from_preamble <- function(licor_exdfs) {
+    lapply(licor_exdfs, function(x) {get_oxygen_info_from_preamble(x)})
 }
 
-get_genotype_info_from_licor_filename <- function(licor_file) {
+get_genotype_info_from_licor_filename <- function(licor_exdf) {
     # Add some new columns to the Licor file in preparation for adding the plant
     # information
-    licor_file <- specify_variables(
-        licor_file,
+    licor_exdf <- specify_variables(
+        licor_exdf,
         c("plant specification", "genotype",                 "NA"),
         c("plant specification", "event",                    "NA"),
         c("plant specification", "replicate",                "NA"),
@@ -128,7 +95,7 @@ get_genotype_info_from_licor_filename <- function(licor_file) {
     )
 
     # Get the filename without the path
-    name <- basename(licor_file[['file_name']])
+    name <- basename(licor_exdf[['file_name']])
 
     # There are two naming conventions we need to check
 
@@ -139,7 +106,7 @@ get_genotype_info_from_licor_filename <- function(licor_file) {
     # filename to end with ' XXX-YYY-ZZZ.xlsx', where `XXX` is the genotype,
     # `YYY` is the event, and `ZZZ` is the replicate.
     plant_specification1 <-
-        regmatches(name, regexpr(" [[:alnum:]]+-[[:alnum:]]+-[[:alnum:]]+\\.", name))
+        regmatches(name, regexpr(" [[:alnum:]]+-[[:alnum:]]+-[[:alnum:]]+\\.xlsx", name))
 
     # Search for the following pattern: one space, followed by one or
     # more alphanumeric characters, followed by a space, followed by one or more
@@ -154,7 +121,7 @@ get_genotype_info_from_licor_filename <- function(licor_file) {
     if (length(plant_specification1) == 0 & length(plant_specification2) == 0) {
         msg <- paste0(
             "Could not extract plant specification information from Licor file:\n'",
-            licor_file[['file_name']],
+            licor_exdf[['file_name']],
             "'\nThe filename must end with ` GGG-EEE-RRR.xlsx` or ` GGG EEE RRR.xlsx`, ",
             "where `GGG`, `EEE`, and `RRR` are alphanumeric specifiers for ",
             "the genotype, event, and replicate represented by the file"
@@ -191,23 +158,18 @@ get_genotype_info_from_licor_filename <- function(licor_file) {
     }
 
     # Store the info in the file and return it
-    licor_file[['main_data']][['genotype']] <- g
-    licor_file[['main_data']][['event']] <- e
-    licor_file[['main_data']][['replicate']] <- r
-    licor_file[['main_data']][['genotype_event']] <- paste0(g, "-", e)
-    licor_file[['main_data']][['event_replicate']] <- paste0(e, "-", r)
-    licor_file[['main_data']][['genotype_event_replicate']] <- paste0(g, "-", e, "-", r)
-    licor_file[['main_data']][['original_file']] <- licor_file[['file_name']]
-    return(licor_file)
+    licor_exdf[,'genotype'] <- g
+    licor_exdf[,'event'] <- e
+    licor_exdf[,'replicate'] <- r
+    licor_exdf[,'genotype_event'] <- paste0(g, "-", e)
+    licor_exdf[,'event_replicate'] <- paste0(e, "-", r)
+    licor_exdf[,'genotype_event_replicate'] <- paste0(g, "-", e, "-", r)
+    licor_exdf[,'original_file'] <- licor_exdf[['file_name']]
+    return(licor_exdf)
 }
 
-batch_get_genotype_info_from_licor_filename <- function(licor_files) {
-    lapply(
-        licor_files,
-        function(licor_file) {
-            get_genotype_info_from_licor_filename(licor_file)
-        }
-    )
+batch_get_genotype_info_from_licor_filename <- function(licor_exdfs) {
+    lapply(licor_exdfs, function(x) {get_genotype_info_from_licor_filename(x)})
 }
 
 pair_licor_and_tdl <- function(
