6552470f7682eabd552c169f2b9c7cd07b5295b7
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-03-08 10:25:54 -0600

    Consolidate scripts into single files

--- example_scripts/c4_induction_curve_analysis.R
+++ example_scripts/c4_induction_curve_analysis.R
@@ -30,9 +30,6 @@
 # relative paths, they should be specified relative to the directory that
 # contains this script.
 #
-# To generate figures based on the analysis performed in this script, see
-# `plot_c4_response_curve_analysis.R`.
-#
 # ------------------------------------------------------------------------------
 #
 # To run the script, set the R working directory to the directory that contains
@@ -41,6 +38,8 @@
 # source('c4_response_curve_analysis.R')
 
 library(PhotoGEA)
+library(lattice)
+library(RColorBrewer)
 
 ###                                                                   ###
 ### COMPONENTS THAT MIGHT NEED TO CHANGE EACH TIME THIS SCRIPT IS RUN ###
@@ -96,7 +95,7 @@ if (PERFORM_CALCULATIONS) {
 # Specify which measurement numbers to choose. Here, the numbers refer to
 # points along the sequence of A-Ci measurements.
 #
-# 
+#
 NUM_OBS_IN_SEQ <- 390
 MEASUREMENT_NUMBERS <- c(1:390)
 POINT_FOR_BOX_PLOTS <- 1
@@ -260,3 +259,177 @@ if (VIEW_DATA_FRAMES) {
     View(all_samples)
     View(all_stats)
 }
+
+###                            ###
+### PLOT RESPONSE CURVES TO CI ###
+###                            ###
+
+rc_caption <- "Average response curves for each event"
+
+# Choose colors for the different events to use when plotting average A-Ci
+# curves. To see other available palettes, use one of the following commands:
+#  display.brewer.all(colorblindFriendly = TRUE)
+#  display.brewer.all(colorblindFriendly = FALSE)
+rc_cols <- c(
+    "#000000",
+    brewer.pal(12, "Paired")[c(1:10,12)],
+    brewer.pal(8, "Set2"),
+    brewer.pal(8, "Dark2")
+)
+rc_cols <- rc_cols[1:length(unique(all_stats_subset[[EVENT_COLUMN_NAME]]))]
+rc_cols <- rev(rc_cols)
+
+# Make a slightly different version of the color specification to use for the
+# error bars
+rc_error_cols <- rep(rc_cols, each=length(MEASUREMENT_NUMBERS))
+
+# Set the line width to use for plotting average response curves. (This will
+# only apply if type is 'l' or 'b' in the calls to xyplot below.)
+line_width <- 2
+
+# Plot the average induction curves
+induction_curves <- xyplot(
+    all_stats_subset[['A_avg']] ~ all_stats_subset[['elapsed_time']],
+    group = all_stats_subset[[EVENT_COLUMN_NAME]],
+    type = 'l',
+    pch = 16,
+    lwd = line_width,
+    auto = TRUE,
+    grid = TRUE,
+    main = rc_caption,
+    xlab = "Elapsed time (minutes)",
+    #xlab = "Intercellular [CO2] (ppm)\n(error bars: standard error of the mean)",
+    ylab = "Net CO2 assimilation rate (micromol / m^2 / s)\n(error bars: standard error of the mean for same CO2 setpoint)",
+    ylim = c(-10, 60),
+    #xlim = c(-50, 1300),
+    par.settings=list(
+        superpose.line=list(col=rc_cols),
+        superpose.symbol=list(col=rc_cols)
+    ),
+    panel = function(x, y, ...) {
+        #panel.arrows(x, y, x, all_stats_subset[['A_upper']], length = 0.05, angle = 90, col = rc_error_cols, lwd = line_width)
+        #panel.arrows(x, y, x, all_stats_subset[['A_lower']], length = 0.05, angle = 90, col = rc_error_cols, lwd = line_width)
+        panel.xyplot(x, y, ...)
+    }
+)
+
+x11(width = 8, height = 6)
+print(induction_curves)
+
+
+###                                     ###
+### PLOT ALL INDIVIDUAL INDUCTION CURVES ###
+###                                     ###
+
+ind_caption <- "Individual induction curves for each event and rep"
+
+num_reps <- length(unique(all_samples_subset[[REP_COLUMN_NAME]]))
+
+# Choose colors for the different reps to use when plotting individual response
+# curves. To see other available palettes, use one of the following commands:
+#  display.brewer.all(colorblindFriendly = TRUE)
+#  display.brewer.all(colorblindFriendly = FALSE)
+ind_cols <- c(
+    "#000000",
+    brewer.pal(12, "Paired"),
+    brewer.pal(8, "Set2"),
+    brewer.pal(8, "Dark2")
+)
+
+# Plot each individual response curve, where each event will have multiple traces
+# corresponding to different plants
+multi_induction_curves <- xyplot(
+    all_samples_subset[['A']] ~ all_samples_subset[['elapsed_time']] | all_samples_subset[[EVENT_COLUMN_NAME]],
+    group = all_samples_subset[[REP_COLUMN_NAME]],
+    type = 'b',
+    pch = 20,
+    auto.key = list(space = "right"),
+    grid = TRUE,
+    main = ind_caption,
+    xlab = "Elapsed time (minutes)",
+    ylab = "Net CO2 assimilation rate (micromol / m^2 / s)",
+    ylim = c(-10, 60),
+    #xlim = c(-100, 1600),
+    par.settings=list(
+        superpose.line=list(col=ind_cols),
+        superpose.symbol=list(col=ind_cols)
+    )
+)
+
+x11(width = 8, height = 6)
+print(multi_induction_curves)
+
+
+
+###                                                    ###
+### MAKE BOX-WHISKER PLOTS FOR last MEASUREMENT POINT ###
+###                                                    ###
+
+boxplot_caption <- paste0(
+    "Quartiles for measurement point ",
+    POINT_FOR_BOX_PLOTS,
+    "\n(where CO2_r_sp = ",
+    all_samples_one_point[['CO2_r_sp']][1],
+    ")"
+)
+
+a_boxplot <- bwplot(
+    all_samples_one_point[['A']] ~ all_samples_one_point[[EVENT_COLUMN_NAME]],
+    ylab = "Net CO2 assimilation rate (micromol / m^2 / s)",
+    ylim = c(0, 70),
+    main = boxplot_caption,
+    xlab = "Genotype"
+)
+
+x11(width = 6, height = 6)
+print(a_boxplot)
+
+phips2_boxplot <- bwplot(
+    all_samples_one_point[['PhiPS2']] ~ all_samples_one_point[[EVENT_COLUMN_NAME]],
+    ylab = "Photosystem II operating efficiency (dimensionless)",
+    ylim = c(0, 0.4),
+    main = boxplot_caption,
+    xlab = "Genotype"
+)
+
+x11(width = 6, height = 6)
+print(phips2_boxplot)
+
+
+etr_boxplot <- bwplot(
+    all_samples_one_point[['ETR']] ~ all_samples_one_point[[EVENT_COLUMN_NAME]],
+    ylab = "Electron transport rate (micromol / m^2 / s)",
+    ylim = c(0, 275),
+    main = boxplot_caption,
+    xlab = "Genotype"
+)
+
+x11(width = 6, height = 6)
+print(etr_boxplot)
+
+###                                             ###
+### MAKE BAR CHARTS FOR FIRST MEASUREMENT POINT ###
+###                                             ###
+
+barchart_caption <- paste0(
+    "Averages for measurement point ",
+    POINT_FOR_BOX_PLOTS,
+    "\n(where CO2_r_sp = ",
+    all_stats_one_point[['CO2_r_sp_avg']][1],
+    ")"
+)
+
+assimilation_barchart <- barchart(
+    all_stats_one_point[['A_avg']] ~ all_stats_one_point[[EVENT_COLUMN_NAME]],
+    ylim = c(0, 60),
+    ylab = "Net CO2 assimilation rate (micromol / m^2 / s)",
+    main = barchart_caption,
+    panel = function(x, y, ..., subscripts) {
+        panel.barchart(x, y, subscripts = subscripts, ...)
+        panel.arrows(x, y, x, all_stats_one_point[['A_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        panel.arrows(x, y, x, all_stats_one_point[['A_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+    }
+)
+
+x11(width = 6, height = 6)
+print(assimilation_barchart)
--- example_scripts/gm_analysis.R
+++ example_scripts/gm_analysis.R
@@ -19,9 +19,6 @@
 # - Components that are less likely to change each time this script is run
 # - The commands that actually call the functions to perform the analysis.
 #
-# To generate figures based on the analysis performed in this script, see
-# `plot_gm_analysis.R`.
-#
 # ------------------------------------------------------------------------------
 #
 # To run the script, set the R working directory to the directory that contains
@@ -30,6 +27,7 @@
 # source('gm_analysis.R')
 
 library(PhotoGEA)
+library(lattice)
 library(onewaytests)  # for bf.test, shapiro.test, A.aov
 library(DescTools)    # for DunnettTest
 
@@ -42,6 +40,10 @@ PERFORM_CALCULATIONS <- TRUE
 PERFORM_STATS_TESTS <- FALSE
 
 SAVE_RESULTS <- TRUE
+
+MAKE_TDL_PLOTS <- TRUE
+
+MAKE_GM_PLOTS <- TRUE
 
 RESPIRATION <- -2.2
 
@@ -349,4 +351,321 @@ if (SAVE_RESULTS) {
     write.csv(licor_files_no_outliers, file.path(base_dir, "gm_calculations_outliers_excluded.csv"), row.names=FALSE)
     write.csv(event_stats, file.path(base_dir, "gm_stats_by_event_outliers_excluded.csv"), row.names=FALSE)
     write.csv(rep_stats, file.path(base_dir, "gm_stats_by_rep_outliers_excluded.csv"), row.names=FALSE)
+}
+
+###                            ###
+### MAKE TDL PLOTS, IF DESIRED ###
+###                            ###
+
+if (MAKE_TDL_PLOTS) {
+    # Make a plot of all the fits from the processing
+    tdl_fitting <- xyplot(
+        expected_13c_values + fitted_13c_values ~ measured_13c_values | factor(cycle_num),
+        data = processed_tdl_data[['calibration_13CO2_data']],
+        type = 'b',
+        pch = 20,
+        xlab = "Measured 13CO2 mixing ratio (ppm)",
+        ylab = "True 13CO2 mixing ratio (ppm)",
+        auto = TRUE
+    )
+    x11(width = 12, height = 6)
+    print(tdl_fitting)
+
+    # Make a plot showing how the zeroes drift with time
+    tdl_12CO2_zero_drift <- xyplot(
+        offset_12c ~ cycle_num,
+        data = processed_tdl_data[['calibration_zero']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "12CO2 zero offset"
+    )
+    x11()
+    print(tdl_12CO2_zero_drift)
+
+    tdl_13CO2_zero_drift <- xyplot(
+        offset_13c ~ cycle_num,
+        data = processed_tdl_data[['calibration_zero']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "13CO2 zero offset"
+    )
+    x11()
+    print(tdl_13CO2_zero_drift)
+
+    # Make a plot showing how the 12CO2 calibration factor drifts with time
+    tdl_12CO2_calibration_drift <- xyplot(
+        gain_12CO2 ~ cycle_num,
+        data = processed_tdl_data[['calibration_12CO2']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "12CO2 gain factor"
+    )
+    x11()
+    print(tdl_12CO2_calibration_drift)
+
+    # Make plots showing how the 13CO2 calibration parameters drift with time
+    tdl_13CO2_calibration_drift_a0 <- xyplot(
+        a0 ~ cycle_num,
+        data = processed_tdl_data[['calibration_13CO2_fit']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "13CO2 polynomial calibration parameter a0",
+        auto = TRUE,
+        main = "true_13CO2 =\na0 +\na1 * measured_13CO2 +\na2 * measured_13CO2^2"
+    )
+    x11()
+    print(tdl_13CO2_calibration_drift_a0)
+
+    tdl_13CO2_calibration_drift_a1 <- xyplot(
+        a1 ~ cycle_num,
+        data = processed_tdl_data[['calibration_13CO2_fit']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "13CO2 polynomial calibration parameter a1",
+        auto = TRUE,
+        main = "true_13CO2 =\na0 +\na1 * measured_13CO2 +\na2 * measured_13CO2^2"
+    )
+    x11()
+    print(tdl_13CO2_calibration_drift_a1)
+
+
+    tdl_13CO2_calibration_drift_a2 <- xyplot(
+        a2 ~ cycle_num,
+        data = processed_tdl_data[['calibration_13CO2_fit']],
+        type = 'l',
+        xlab = "TDL cycle number",
+        ylab = "13CO2 polynomial calibration parameter a2",
+        auto = TRUE,
+        main = "true_13CO2 =\na0 +\na1 * measured_13CO2 +\na2 * measured_13CO2^2"
+    )
+    x11()
+    print(tdl_13CO2_calibration_drift_a2)
+}
+
+###                           ###
+### MAKE GM PLOTS, IF DESIRED ###
+###                           ###
+
+if (MAKE_GM_PLOTS) {
+    # Convert some columns to factors so we can control the order of boxes and
+    # bars when plotting
+    licor_files_no_outliers_data <- licor_files_no_outliers[['main_data']]
+
+    licor_files_no_outliers_data[['event']] <- factor(
+        licor_files_no_outliers_data[['event']],
+        levels = sort(
+            unique(licor_files_no_outliers_data[['event']]),
+            decreasing = TRUE
+        )
+    )
+
+    licor_files_no_outliers_data[['event_replicate']] <- factor(
+        licor_files_no_outliers_data[['event_replicate']],
+        levels = sort(
+            unique(licor_files_no_outliers_data[['event_replicate']]),
+            decreasing = TRUE
+        )
+    )
+
+    event_stats[['event']] <- factor(
+        event_stats[['event']],
+        levels = sort(
+            unique(event_stats[['event']]),
+            decreasing = TRUE
+        )
+    )
+
+    # Make gmc boxplots for each event
+    gmc_event_boxplot <- bwplot(
+        gmc ~ event | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, MAX_GM),
+        ylab = "Mesophyll conductance to CO2 (mol / m^2 / s / bar)"
+    )
+    x11()
+    print(gmc_event_boxplot)
+
+    # Make gmc boxplots for each rep
+    gmc_rep_boxplot <- bwplot(
+        gmc ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, MAX_GM),
+        ylab = "Mesophyll conductance to CO2 (mol / m^2 / s / bar)"
+    )
+    x11()
+    print(gmc_rep_boxplot)
+
+    # Make gmc bars for each event
+    gmc_barchart <- barchart(
+        gmc_avg ~ event,
+        data = event_stats,
+        ylim = c(0,1),
+        ylab = "Mesophyll conductance to CO2 (mol / m^2 / s / bar)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['gmc_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['gmc_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(gmc_barchart)
+
+    # Make Cc boxplots for each event
+    cc_event_boxplot <- bwplot(
+        Cc ~ event | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 275),
+        ylab = "CO2 concentration in chloroplast (micromol / mol)"
+    )
+    x11()
+    print(cc_event_boxplot)
+
+    # Make Cc boxplots for each rep
+    cc_rep_boxplot <- bwplot(
+        Cc ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 275),
+        ylab = "CO2 concentration in chloroplast (micromol / mol)"
+    )
+    x11()
+    print(cc_rep_boxplot)
+
+    # Make Cc bars for each event
+    cc_barchart <- barchart(
+        Cc_avg ~ event,
+        data = event_stats,
+        ylim = c(0, 275),
+        ylab = "CO2 concentration in chloroplast (micromol / mol)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['Cc_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['Cc_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(cc_barchart)
+
+    # Make drawdown boxplots for each event
+    drawdown_event_boxplot <- bwplot(
+        `Ci-Cc` ~ event | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 150),
+        ylab = "CO2 drawdown to chloroplast (Ci - Cc) (micromol / mol)"
+    )
+    x11()
+    print(drawdown_event_boxplot)
+
+    # Make drawdown boxplots for each rep
+    drawdown_boxplot <- bwplot(
+        `Ci-Cc` ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 150),
+        ylab = "CO2 drawdown to chloroplast (Ci - Cc) (micromol / mol)"
+    )
+    x11()
+    print(drawdown_boxplot)
+
+    # Make drawdown bars for each event
+    drawdown_barchart <- barchart(
+        `Ci-Cc_avg` ~ event,
+        data = event_stats,
+        ylim = c(0, 80),
+        ylab = "CO2 drawdown to chloroplast (Ci - Cc) (micromol / mol)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['Ci-Cc_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['Ci-Cc_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(drawdown_barchart)
+
+    # Make assimilation boxplots for each event
+    assimilation_event_boxplot <- bwplot(
+        A ~ event | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 50),
+        ylab = "Net CO2 assimilation rate (micromol / m^2 / s)"
+    )
+    x11()
+    print(assimilation_event_boxplot)
+
+    # Make assimilation boxplots for each rep
+    assimilation_boxplot <- bwplot(
+        A ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 50),
+        ylab = "Net CO2 assimilation rate (micromol / m^2 / s)"
+    )
+    x11()
+    print(assimilation_boxplot)
+
+    # Make assimilation bars for each event
+    assimilation_barchart <- barchart(
+        A_avg ~ event,
+        data = event_stats,
+        ylim = c(0, 50),
+        ylab = "Net CO2 assimilation rate (micromol / m^2 / s)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['A_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['A_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(assimilation_barchart)
+
+    # Make iWUE boxplots for each rep
+    iWUE_boxplot <- bwplot(
+        iWUE ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 120),
+        ylab = "Intrinsic water use efficiency (micromol CO2 / mol H2O)"
+    )
+    x11()
+    print(iWUE_boxplot)
+
+    # Show iWUE time series
+    #xyplot(licor_files_no_outliers[,'iWUE'] ~ as.numeric(row.names(licor_files_no_outliers[['main_data']])), group = licor_files_no_outliers[,'event'], type = 'p', auto = TRUE)
+
+    # Make iWUE bars for each event
+    iwue_barchart <- barchart(
+        iWUE_avg ~ event,
+        data = event_stats,
+        ylim = c(0, 120),
+        ylab = "Intrinsic water use efficiency (micromol CO2 / mol H2O)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['iWUE_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['iWUE_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(iwue_barchart)
+
+    # Make g_ratio boxplots for each rep
+    g_ratio_boxplot <- bwplot(
+        g_ratio ~ event_replicate | genotype,
+        data = licor_files_no_outliers_data,
+        ylim = c(0, 1),
+        ylab = "Ratio of stomatal / mesophyll conductances to CO2 (gs / gm; dimensionless)"
+    )
+    x11()
+    print(g_ratio_boxplot)
+
+    # Make iWUE bars for each event
+    g_ratio_barchart <- barchart(
+        g_ratio_avg ~ event,
+        data = event_stats,
+        ylim = c(0, 0.6),
+        ylab = "Ratio of stomatal / mesophyll conductances to CO2 (gs / gm; dimensionless)",
+        panel = function(x, y, ..., subscripts) {
+            panel.barchart(x, y, subscripts = subscripts, ...)
+            panel.arrows(x, y, x, event_stats[['g_ratio_upper']], length = 0.2, angle = 90, col = "black", lwd = 1)
+            panel.arrows(x, y, x, event_stats[['g_ratio_lower']], length = 0.2, angle = 90, col = "black", lwd = 1)
+        }
+    )
+    x11()
+    print(g_ratio_barchart)
+
 }
