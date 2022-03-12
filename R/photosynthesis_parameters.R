10516536a18ad51ea252aa4c72f41ee85fbb6a74
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-03-11 18:11:02 -0600

    Add von Caemmerer C4 photosynthesis parameter temperature response function

--- R/photosynthesis_parameters.R
+++ R/photosynthesis_parameters.R
@@ -1,17 +1,85 @@
+# Define a few useful constants for the Arrhenius function
+ideal_gas_constant <- 8.3145e-3  # kJ / mol / k
+absolute_zero <- -273.15         # degrees C
+
 # A helping function for calculating Arrhenius exponent factors.
+#
+# Note: If the Arrhenius exponent should be X at 25 degrees C, then we have the
+# following: X = exp(scaling - enthalpy / (ideal_gas_constant * (25 + 273.15))),
+# so log(X) = scaling - enthalpy / (ideal_gas_constant * (25 + 273.15)),
+# so scaling = log(X) + enthalpy / (ideal_gas_constant * (25 + 273.15)),
+# so scaling = log(X) + enthalpy / f, where
+# f = ideal_gas_constant * (25 + 273.15).
+#
+# As a special case, for parameters normalized to 1 at 25 degrees C, we have
+# scaling = enthalpy / f.
+#
+# Note: Let's say Y is determined by an Arrhenius exponent, i.e., that
+# Y = exp(c - g(T)), and we want to convert Y to different units via a
+# multiplicative conversion factor cf. Then, in the new units, Y becomes
+# Y_new = cf * Y = cf * exp(c - g(T)) = exp(c + log(cf) - g(T)) =
+# exp(c_new - g(T)), where c_new = c + log(cf). So we can continue to calculate
+# Y_new using an Arrhenius function but with a different scaling factor.
+#
 arrhenius <- function(
     scaling,     # dimensionless
     enthalpy,    # kJ / mol
-    temperature  # Degrees C
+    temperature  # degrees C
 )
 {
-    ideal_gas_constant <- 8.3145e-3  # kJ / mol / k
-    absolute_zero <- -273.15         # Degrees C
-
     temperature_k <- temperature - absolute_zero # Kelvin
 
     return(exp(scaling - enthalpy / (ideal_gas_constant * temperature_k)))
 }
+
+#-------------------------------------------------------------------------------
+
+# Define a few additional useful constants (see the comments above `arrhenius`
+# for more details).
+f <- ideal_gas_constant * (25 - absolute_zero)
+c_pa_to_ppm <- log(1e6 / 101325)
+
+# Define Arrhenius parameters for the C3 Bernacchi temperature response
+# function; here c (dimensionless) is the scaling factor and dha (kJ / mol) is
+# the enthalpy.
+temperature_response_parameters_Bernacchi <- list(
+    Rd =         list(c = 18.72, dha = 46.39),  # dimensionless (normalized to Rd at 25 degrees C)
+    Vcmax =      list(c = 26.35, dha = 65.33),  # dimensionless (normalized to Vcmax at 25 degrees C)
+    Vomax =      list(c = 22.98, dha = 60.11),  # dimensionless (normalized to Vcmax at 25 degrees C)
+    Gamma_star = list(c = 19.02, dha = 37.83),  # micromol / mol
+    Kc =         list(c = 38.05, dha = 79.43),  # micromol / mol
+    Ko =         list(c = 20.30, dha = 36.38),  # mmol / mol
+    J =          list(c = 17.57, dha = 43.5)    # dimensionless (normalized to J at 25 degrees C)
+)
+
+# Define Arrhenius parameters for the C3 Sharkey temperature response
+# function; here c (dimensionless) is the scaling factor and dha (kJ / mol) is
+# the enthalpy.
+temperature_response_parameters_Sharkey <- list(
+    Kc =         list(c = 35.9774 + c_pa_to_ppm, dha = 80.99),  # micromol / mol
+    Ko =         list(c = 12.3772 + c_pa_to_ppm, dha = 23.72),  # mmol / mol
+    Gamma_star = list(c = 11.187  + c_pa_to_ppm, dha = 24.46),  # micromol / mol
+    Vcmax =      list(c = 26.355,                dha = 65.33),  # dimensionless (normalized to Vcmax at 25 degrees C)
+    J =          list(c = 17.71,                 dha = 43.9),   # dimensionless (normalized to J at 25 degrees C)
+    Rd =         list(c = 18.7145,               dha = 46.39)   # dimensionless (normalized to Rd at 25 degrees C)
+)
+
+# Define Arrhenius parameters for the C4 von Caemmerer temperature response
+# function; here c (dimensionless) is the scaling factor and dha (kJ / mol) is
+# the enthalpy.
+temperature_response_parameters_von_Caemmerer <- list(
+    Vcmax =      list(c = 78 / f,                      dha = 78),    # dimensionless (normalized to Vcmax at 25 degrees C)
+    Vpmax =      list(c = 50.1 / f,                    dha = 50.1),  # dimensionless (normalized to Vpmax at 25 degrees C)
+    Rd =         list(c = log(0.01) + 66.4 / f,        dha = 66.4),  # dimensionless (normalized to Vcmax at 25 degrees C)
+    Kc =         list(c = log(1210) + 64.2 / f,        dha = 64.2),  # microbar
+    Ko =         list(c = log(292) + 10.5 / f,         dha = 10.5),  # mbar
+    Kp =         list(c = log(82) + 38.3 / f,          dha = 38.3),  # microbar
+    gamma_star = list(c = log(0.5 / 1310) + 31.1 / f,  dha = 31.1),  # dimensionless
+    ao =         list(c = log(0.047) + 1.63 / f,       dha = 1.63),  # dimensionless
+    gm =         list(c = log(1) + 49.8 / f,           dha = 49.8)   # mol / m^2 / s / bar
+)
+
+#-------------------------------------------------------------------------------
 
 # Calculate the temperature-dependent values of the following photosynthesis
 # parameters: Rd, Vcmax, Vomax, Gamma_star, Kc, Ko, J.
@@ -45,24 +113,12 @@ arrhenius <- function(
 # (https://doi.org/10.1046/j.0016-8025.2003.01050.x)
 #
 c3_photosynthesis_parameters_Bernacchi <- function(
-    leaf_temperature  # Degrees C
+    leaf_temperature  # degrees C
 )
 {
-    # Define scaling factors (c; dimensionless) and enthalpy values
-    # (dha; kJ / mol) to use for calculating temperature-corrected values.
-    temperature_response_parameters <- list(
-        Rd =         list(c = 18.72, dha = 46.39),  # dimensionless
-        Vcmax =      list(c = 26.35, dha = 65.33),  # dimensionless
-        Vomax =      list(c = 22.98, dha = 60.11),  # dimensionless
-        Gamma_star = list(c = 19.02, dha = 37.83),  # micromol / mol
-        Kc =         list(c = 38.05, dha = 79.43),  # micromol / mol
-        Ko =         list(c = 20.30, dha = 36.38),  # mmol / mol
-        J =          list(c = 17.57, dha = 43.5)    # dimensionless
-    )
-
     return(
         lapply(
-            temperature_response_parameters,
+            temperature_response_parameters_Bernacchi,
             function(x) {arrhenius(x$c, x$dha, leaf_temperature)}
         )
     )
@@ -88,11 +144,7 @@ c3_photosynthesis_parameters_Bernacchi <- function(
 # concentration expressed as a mole fraction (C; in micromol / mol), we need a
 # value for atmospheric pressure; we will use the typical value of 101325 Pa.
 # Then C = P / 101325 * 1e6 = P * cf, where cf = 1e6 / 101325 is a conversion
-# factor. If P is calculated using an Arrhenius exponent, i.e., if
-# P = exp(c - f(T)), then C = cf * exp(c - f(T)) = exp(c + log(cf) - f(T)).
-# Thus, we can convert units from Pa to micromol / mol by adding log(cf) to the
-# scaling factor c. The same correction can be used to convert kPa to
-# mmol / mol.
+# factor. The same correction can be used to convert kPa to mmol / mol.
 #
 # References:
 #
@@ -102,27 +154,42 @@ c3_photosynthesis_parameters_Bernacchi <- function(
 # (https://doi.org/10.1111/j.1365-3040.2007.01710.x)
 #
 c3_photosynthesis_parameters_Sharkey <- function(
-    leaf_temperature  # Degrees C
+    leaf_temperature  # degrees C
 )
 {
-    # Calculate a correction term to add to the scaling factor values as
-    # reported in Sharkey et al. (2007).
-    correction <- log(1e6 / 101325)
-
-    # Define scaling factors (c; dimensionless) and enthalpy values
-    # (dha; kJ / mol) to use for calculating temperature-corrected values.
-    temperature_response_parameters <- list(
-        Kc =         list(c = 35.9774 + correction, dha = 80.99),  # micromol / mol
-        Ko =         list(c = 12.3772 + correction, dha = 23.72),  # mmol / mol
-        Gamma_star = list(c = 11.187  + correction, dha = 24.46),  # micromol / mol
-        Vcmax =      list(c = 26.355,               dha = 65.33),  # dimensionless
-        J =          list(c = 17.71,                dha = 43.9),   # dimensionless
-        Rd =         list(c = 18.7145,              dha = 46.39)   # dimensionless
-    )
-
     return(
         lapply(
-            temperature_response_parameters,
+            temperature_response_parameters_Sharkey,
+            function(x) {arrhenius(x$c, x$dha, leaf_temperature)}
+        )
+    )
+}
+
+# Calculate the temperature-dependent values of the following photosynthesis
+# parameters: Vcmax, Vpmax, Rd, Kc, Ko, Kp, gamma_star, ao, and gm.
+#
+# Some of these parameters (Vcmax and Vpmax) are normalized to their values
+# at 25 degrees C. Rd is normalized to the value of Vcmax at 25 degrees C.
+# The remaining parameters are not normalized because they are assumed to not
+# vary significantly between species.
+#
+# Here we use Arrhenius functions to calculate the temperature-dependent values,
+# where the scaling factors (c; dimensionless) and enthalpy values
+# (dha; kJ / mol) are obtained from von Caemmerer (2021).
+#
+# References:
+#
+# von Caemmerer, S. "Updating the steady-state model of C4 photosynthesis"
+# Journal of Experimental Botany 72, 6003–6017 (2021)
+# (https://doi.org/10.1093/jxb/erab266)
+#
+c4_photosynthesis_parameters_von_Caemmerer <- function(
+    leaf_temperature  # degrees C
+)
+{
+    return(
+        lapply(
+            temperature_response_parameters_von_Caemmerer,
             function(x) {arrhenius(x$c, x$dha, leaf_temperature)}
         )
     )
