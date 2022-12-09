0e105785e8dfe793826e5ee1ec11e400a1148cf2
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-12-08 21:05:57 -0600

    Automatically locate the data block in Licor Excel files

--- R/read_licor.R
+++ R/read_licor.R
@@ -89,71 +89,32 @@ read_licor_6800_Excel <- function(
     data_start_row
 )
 {
-    # Define a helping function for reading one row (with column names) from an
-    # Excel file, where the data row is specified and the column names are
-    # assumed to be in the preceding row
-    read_row_wc <- function(row_num)
-    {
-        row_data <- openxlsx::readWorkbook(
-            file_name,
-            colNames = TRUE,
-            skipEmptyCols = FALSE,
-            rows = c(row_num - 1, row_num)
-        )
-    }
-
-    # Get the preamble data rows
-    licor_preamble <- lapply(preamble_data_rows, read_row_wc)
-
-    # Define a helping function for reading one row (without column names) from
-    # an Excel file
-    read_row_nc <- function(row_num)
-    {
-        row_data <- openxlsx::readWorkbook(
-            file_name,
-            colNames = FALSE,
-            skipEmptyCols = FALSE,
-            rows = row_num
-        )
-    }
-
-    # Define a helping function for converting a row to a data frame
-    row_to_data_frame <- function(rowdata)
-    {
-        data_frame <- data.frame(
-            matrix(
-                ncol = length(rowdata),
-                nrow = 1
-            ),
-            stringsAsFactors = FALSE
-        )
-        data_frame[1,] <- rowdata
-        return(data_frame)
-    }
-
-    # Define a helping function for reading a row, replacing any Unicode
-    # characters, and converting the result to a data frame
-    get_processed_row_data_frame <- function(row_num)
-    {
-        row_data <- read_row_nc(row_num)
-        row_data <- replace_unicode(row_data)
-        row_data <- row_to_data_frame(row_data)
-        return(row_data)
-    }
-
-    # Get the column types, names and units, replacing any problematic Unicode
-    # characters and converting the results to data frames when required
-    licor_variable_categories <- get_processed_row_data_frame(variable_category_row)
-    licor_variable_names <- get_processed_row_data_frame(variable_name_row)
-    licor_variable_units <- get_processed_row_data_frame(variable_unit_row)
-
-    # Get the main data
-    licor_data <- openxlsx::readWorkbook(
+    # Read the entire workbook into a single data frame
+    rawdata <- openxlsx::readWorkbook(
         file_name,
-        startRow = data_start_row,
         colNames = FALSE,
+        skipEmptyRows = FALSE,
         skipEmptyCols = FALSE
     )
+
+    # Search for the column and row indices that match 'obs'
+    column_name <- 'obs'
+    data_search <- sapply(rawdata, function(x) {match(column_name, x)})
+    data_col <- which(!is.na(data_search))
+    data_row <- data_search[data_col]
+
+    # Get variable names, units, and categories
+    licor_variable_names <- replace_unicode(rawdata[data_row, ])
+
+    licor_variable_units <- as.data.frame(matrix(nrow = 1, ncol = ncol(rawdata)))
+    licor_variable_units[1, ] <- replace_unicode(rawdata[data_row + 1, ])
+    colnames(licor_variable_units) <- licor_variable_names
+
+    licor_variable_categories <- licor_variable_units
+    licor_variable_categories[1, ] <- replace_unicode(rawdata[data_row - 1, ])
+
+    # Get the main data
+    licor_data <- rawdata[seq(data_row + 2, nrow(rawdata)), ]
 
     # Convert the data to numeric values whenever possible
     licor_data <- as.data.frame(
@@ -162,9 +123,27 @@ read_licor_6800_Excel <- function(
     )
 
     # Apply column names
-    colnames(licor_data) <- licor_variable_names[1,]
-    colnames(licor_variable_categories) <- licor_variable_names[1,]
-    colnames(licor_variable_units) <- licor_variable_names[1,]
+    colnames(licor_data) <- licor_variable_names
+    colnames(licor_variable_categories) <- licor_variable_names
+    colnames(licor_variable_units) <- licor_variable_names
+
+    # Get the raw preamble data
+    raw_preamble <- rawdata[seq_len(data_row - 2), ]
+
+    # Define a helping function for extracting one row of the preamble data
+    extract_preamble_row <- function(start_indx) {
+        pr <- as.data.frame(matrix(nrow = 1, ncol = ncol(raw_preamble)))
+        pr[1, ] <- replace_unicode(raw_preamble[start_indx + 1, ])
+        colnames(pr) <- replace_unicode(raw_preamble[start_indx, ])
+        pr[!is.na(colnames(pr))]
+    }
+
+    # Get all the rows and combine them into one data frame
+    row_df_list <- lapply(
+        seq(1, nrow(raw_preamble), by = 2),
+        extract_preamble_row
+    )
+    licor_preamble <- do.call(cbind, row_df_list)
 
     return(
         exdf(
@@ -175,11 +154,7 @@ read_licor_6800_Excel <- function(
             instrument_type = 'Licor LI-6800',
             file_type = 'plaintext',
             preamble = licor_preamble,
-            preamble_data_rows = preamble_data_rows,
-            variable_category_row = variable_category_row,
-            variable_name_row = variable_name_row,
-            variable_unit_row = variable_unit_row,
-            data_start_row = data_start_row
+            data_row = data_row
         )
     )
 }
