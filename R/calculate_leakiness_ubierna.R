87eba188db2f40e86b8d71fe6c07e8986406c314
eloch216 <48919455+eloch216@users.noreply.github.com>
2023-08-14 20:58:04 -0500

    Document `calculate_leakiness_ubierna`

--- R/calculate_leakiness_ubierna.R
+++ R/calculate_leakiness_ubierna.R
@@ -1,95 +1,98 @@
 calculate_leakiness_ubierna <- function(
-    licor_exdf,
-    e = -3 # ppt; isotopic fractionation during day respiration
+    exdf_obj,
+    e = -3, # ppt; isotopic fractionation during day respiration
+    a_bar_column_name = 'a_bar',
+    a_column_name = 'A',
+    ci_column_name = 'Ci',
+    co2_s_column_name = 'CO2_s',
+    csurface_column_name = 'Csurface',
+    delta_c13_r_column_name = 'delta_C13_r',
+    delta_obs_tdl_column_name = 'Delta_obs_tdl',
+    rd_column_name = 'Rd',
+    t_column_name = 't'
 )
 {
-  if (!is.exdf(licor_exdf)) {
-      stop('calculate_leakiness_ubierna requires an exdf object')
-  }
+    if (!is.exdf(exdf_obj)) {
+        stop('calculate_leakiness_ubierna requires an exdf object')
+    }
 
-  # Make sure the required variables are defined and have the correct units
-  required_variables <- list()
-  required_variables$A               <- 'micromol m^(-2) s^(-1)'
-  required_variables$a_bar           <- 'ppt'
-  required_variables$Ci              <- 'micromol mol^(-1)'
-  required_variables$CO2_s           <- 'micromol mol^(-1)'
-  required_variables$CO2_s_corrected <- 'micromol mol^(-1)'
-  required_variables$Csurface        <- 'micromol mol^(-1)'
-  required_variables$delta_C13_r     <- 'ppt'
-  required_variables$Delta_obs_tdl   <- 'ppt'
-  required_variables$Rd              <- 'micromol m^(-2) s^(-1)'
-  required_variables$t               <- 'dimensionless'
+    # Make sure the required variables are defined and have the correct units
+    required_variables <- list()
+    required_variables[[a_bar_column_name]]         <- 'ppt'
+    required_variables[[a_column_name]]             <- 'micromol m^(-2) s^(-1)'
+    required_variables[[ci_column_name]]            <- 'micromol mol^(-1)'
+    required_variables[[co2_s_column_name]]         <- 'micromol mol^(-1)'
+    required_variables[[csurface_column_name]]      <- 'micromol mol^(-1)'
+    required_variables[[delta_c13_r_column_name]]   <- 'ppt'
+    required_variables[[delta_obs_tdl_column_name]] <- 'ppt'
+    required_variables[[rd_column_name]]            <- 'micromol m^(-2) s^(-1)'
+    required_variables[[t_column_name]]             <- 'dimensionless'
 
-  check_required_variables(licor_exdf, required_variables)
+    check_required_variables(exdf_obj, required_variables)
 
-  # Extract some important columns
-  A               <- licor_exdf[, 'A']               # micromol / m^2 / s
-  a_bar           <- licor_exdf[, 'a_bar']           # ppt
-  Ci              <- licor_exdf[, 'Ci']              # micromol / mol
-  CL              <- licor_exdf[, 'Csurface']        # micromol / mol
-  CO2_s           <- licor_exdf[, 'CO2_s']           # micromol / mol
-  CO2_s_corrected <- licor_exdf[, 'CO2_s_corrected'] # micromol / mol
-  delta_C13_r     <- licor_exdf[, 'delta_C13_r']     # ppt
-  Delta_obs_tdl   <- licor_exdf[, 'Delta_obs_tdl']   # ppt
-  Rd              <- licor_exdf[, 'Rd']              # micromol / m^2 / s
-  t               <- licor_exdf[, 't']               # dimensionless
+    # Extract some important columns
+    A             <- exdf_obj[, a_column_name]             # micromol / m^2 / s
+    a_bar         <- exdf_obj[, a_bar_column_name]         # ppt
+    Ci            <- exdf_obj[, ci_column_name]            # micromol / mol
+    CL            <- exdf_obj[, csurface_column_name]      # micromol / mol
+    CO2_s         <- exdf_obj[, co2_s_column_name]         # micromol / mol
+    delta_C13_r   <- exdf_obj[, delta_c13_r_column_name]   # ppt
+    Delta_obs_tdl <- exdf_obj[, delta_obs_tdl_column_name] # ppt
+    Rd            <- exdf_obj[, rd_column_name]            # micromol / m^2 / s
+    t             <- exdf_obj[, t_column_name]             # dimensionless
 
-  # Get the values of some constants that are defined in `constants.R`
-  b_prime_3        <- ISOTOPE_CONSTANTS$b               # ppt
-  b_prime_4        <- ISOTOPE_CONSTANTS$b_prime_4       # ppt
-  delta_13c_growth <- ISOTOPE_CONSTANTS$delta_Ca_growth # ppt
-  s                <- ISOTOPE_CONSTANTS$s               # ppt
+    # Get the values of some constants that are defined in `constants.R`
+    b_prime_3        <- ISOTOPE_CONSTANTS$b               # ppt
+    b_prime_4        <- ISOTOPE_CONSTANTS$b_prime_4       # ppt
+    delta_13c_growth <- ISOTOPE_CONSTANTS$delta_Ca_growth # ppt
+    s                <- ISOTOPE_CONSTANTS$s               # ppt
 
-  # Define some other constants to avoid magic numbers in the equations below
-  Rm_frac <- 0.5         # Rm is defined as a fraction of Rd
+    # Rm is assumed to be proportional to Rd
+    Rm_frac <- 0.5
+    Rm <- Rd * Rm_frac # micromol / m^2 / s
 
-  # Make the calculations
+    # Equation 21 from Ubierna et al. (2013). e_prime is the fractionation
+    # during decarboxylation including measurement artefacts
+    e_prime <- e + delta_C13_r - delta_13c_growth # ppt
 
-  # Rm is assumed to be proportional to Rd
-  Rm <- Rd * Rm_frac # micromol / m^2 / s
+    # The high-light version of Equation 16 from Ubierna et al. (2013). phi_i is
+    # the leakiness estimated with the isotope method including respiratory and
+    # photorespiratory fractionations
+    phi_i_top <-
+        ((1 - t) * Delta_obs_tdl * CO2_s - a_bar * (CO2_s - Ci)) / ((1 + t) * Ci) -
+        b_prime_4 + e_prime * Rm / (A + 0.5 * Rd) # ppt
 
-  # Equation 21 from Ubierna et al. (2012). e_prime is the fractionation during
-  # decarboxylation including measurement artefacts
-  e_prime <- e + delta_C13_r - delta_13c_growth # ppt
+    phi_i_bottom <- b_prime_3 - s + e_prime * (Rm / (A + 0.5 * Rd) - Rd / (A + Rd)) # ppt
 
-  # The high-light version of Equation 16 from Ubierna et al. (2012). phi_i is
-  # the leakiness estimated with the isotope method including respiratory and
-  # photorespiratory fractionations
-  phi_i_top <-
-    ((1 - t) * Delta_obs_tdl * CO2_s_corrected - a_bar * (CO2_s_corrected - Ci)) / ((1 + t) * Ci) -
-      b_prime_4 + e_prime * Rm / (A + 0.5 * Rd) # ppt
+    phi_i <- phi_i_top / phi_i_bottom # dimensionless
 
-  phi_i_bottom <- b_prime_3 - s + e_prime * (Rm / (A + 0.5 * Rd) - Rd / (A + Rd)) # ppt
+    # The high-light version of Equation 15 from Ubierna et al. (2013). phi_is is
+    # the leakiness estimated with the isotope method including respiratory and
+    # photorespiratory fractionations and Cs
+    phi_is <- phi_i # dimensionless
 
-  phi_i <- phi_i_top / phi_i_bottom # dimensionless
+    # Equation 17 from Ubierna et al. (2013). phi_sim is the leakiness estimated
+    # with the isotope method ignoring respiratory and photorespiratory
+    # fractionations and Cs
+    phi_sim_top <- Delta_obs_tdl * (1 - t) * CO2_s -
+        a_bar * (CO2_s - Ci) - (1 + t) * Ci * b_prime_4 # ppt * micromol / mol
 
-  # The high-light version of Equation 15 from Ubierna et al. (2012). phi_is is
-  # the leakiness estimated with the isotope method including respiratory and
-  # photorespiratory fractionations and Cs
-  phi_is <- phi_i # dimensionless
+    phi_sim_bottom <- Ci * (1 + t) * (b_prime_3 - s) # ppt * micromol / mol
 
-  # Equation 17 from Ubierna et al. (2012). phi_sim is the leakiness estimated
-  # with the isotope method ignoring respiratory and photorespiratory
-  # fractionations and Cs
-  phi_sim_top <- Delta_obs_tdl * (1 - t) * CO2_s_corrected -
-    a_bar * (CO2_s_corrected - Ci) - (1 + t) * Ci * b_prime_4 # ppt * micromol / mol
+    phi_sim <- phi_sim_top / phi_sim_bottom # dimensionless
 
-  phi_sim_bottom <- Ci * (1 + t) * (b_prime_3 - s) # ppt * micromol / mol
+    # Store the calculated quantities in the exdf object
+    exdf_obj[, 'e_prime'] <- e_prime
+    exdf_obj[, 'phi_i'] <- phi_i
+    exdf_obj[, 'phi_is'] <- phi_is
+    exdf_obj[, 'phi_sim'] <- phi_sim
 
-  phi_sim <- phi_sim_top / phi_sim_bottom # dimensionless
-
-  # Store the calculated quantities in the exdf object
-  licor_exdf[, 'e_prime'] <- e_prime
-  licor_exdf[, 'phi_i'] <- phi_i
-  licor_exdf[, 'phi_is'] <- phi_is
-  licor_exdf[, 'phi_sim'] <- phi_sim
-
-  # Document the columns that were added and return the exdf
-  document_variables(
-    licor_exdf,
-    c('calculate_leakiness', 'e_prime', 'ppt'),
-    c('calculate_leakiness', 'phi_i',   'dimensionless'),
-    c('calculate_leakiness', 'phi_is',  'dimensionless'),
-    c('calculate_leakiness', 'phi_sim', 'dimensionless')
-  )
+    # Document the columns that were added and return the exdf
+    document_variables(
+        exdf_obj,
+        c('calculate_leakiness', 'e_prime', 'ppt'),
+        c('calculate_leakiness', 'phi_i',   'dimensionless'),
+        c('calculate_leakiness', 'phi_is',  'dimensionless'),
+        c('calculate_leakiness', 'phi_sim', 'dimensionless')
+    )
 }
