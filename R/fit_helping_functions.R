856894f2e5d0d06b61856c44c534b0c71158591c
eloch216 <48919455+eloch216@users.noreply.github.com>
2024-01-11 16:03:04 -0500

    Add `assemble_luf`

--- R/fit_helping_functions.R
+++ R/fit_helping_functions.R
@@ -5,3 +5,99 @@ check_arg_length <- function(target_length, arg_list) {
         stop(arg_names, ' must each have ', target_length, ' elements')
     }
 }
+
+# A helping function that combines user-supplied and default values of `lower`,
+# `upper`, and `fit_options`, as used by several fitting functions in PhotoGEA.
+# This is only intended to be used internally, so checks are not provided for
+# some of the inputs that cannot be affected by user actions.
+assemble_luf <- function(
+    param_names,
+    default_lower,
+    default_upper,
+    default_fit_options,
+    user_lower,
+    user_upper,
+    user_fit_options
+)
+{
+    user_inputs <- list(user_lower, user_upper, user_fit_options)
+
+    # Make sure the user inputs are lists
+    is_list <- sapply(user_inputs, is.list)
+
+    if (any(!is_list)) {
+        stop('`lower`, `upper`, and `fit_options` must be lists')
+    }
+
+    # Make sure the lists have names
+    missing_names <- sapply(user_inputs, function(x) {
+        if (length(x) < 1) {
+            FALSE
+        } else {
+            is.null(names(x))
+        }
+    })
+
+    if (any(missing_names)) {
+        stop('`lower`, `upper`, and `fit_options` must have named elements if they are not empty lists')
+    }
+
+    # Make sure the user list names are acceptable
+    all_names <- c(
+        names(user_lower),
+        names(user_upper),
+        names(user_fit_options)
+    )
+
+    if (any(all_names == '')) {
+        stop('`lower`, `upper`, and `fit_options` must not have any empty names')
+    }
+
+    if (any(!all_names %in% param_names)) {
+        stop(
+            '`lower`, `upper`, and `fit_options` must only provide settings for ',
+            paste0('`', param_names, '`', collapse = ', ')
+        )
+    }
+
+    # Make sure the fit options are acceptable
+    fit_options_okay <- sapply(user_fit_options, function(x) {
+        if (is.numeric(x)) {
+            TRUE
+        } else if (is.character(x)) {
+            tolower(x) %in% c('fit', 'column')
+        } else {
+            FALSE
+        }
+    })
+
+    if (any(!fit_options_okay)) {
+        stop('Each element of `fit_options` must be `fit`, `column`, or a numeric value')
+    }
+
+    # Combine the default and user lists
+    default_lower[names(user_lower)] <- user_lower
+    default_upper[names(user_upper)] <- user_upper
+    default_fit_options[names(user_fit_options)] <- user_fit_options
+
+    # Make sure capitalization is standardized in fit options
+    default_fit_options <- lapply(default_fit_options, function(x) {
+        if (is.numeric(x)) {
+            x
+        } else {
+            tolower(x)
+        }
+    })
+
+    # Make sure elements are properly ordered
+    default_lower <- default_lower[param_names]
+    default_upper <- default_upper[param_names]
+    default_fit_options <- default_fit_options[param_names]
+
+    # Return
+    list(
+        lower = default_lower,
+        upper = default_upper,
+        fit_options = default_fit_options
+    )
+}
---
+++ tests/testthat/test-assemble_luf.R
@@ -0,0 +1,113 @@
+# Set some parameter names to use for testing
+test_param_names <- c('alpha', 'Rd', 'Vcmax', 'TPU')
+
+test_that('reasonable inputs work properly', {
+    res <- PhotoGEA:::assemble_luf(
+        test_param_names,
+        default_lower = list(alpha = 0, Rd = 0, Vcmax = 0, TPU = 0),
+        default_upper = list(TPU = 100, alpha = 1, Rd = 10, Vcmax = 1000),
+        default_fit_options = list(alpha = 0, Rd = 'fit', Vcmax = 'column', TPU = 40),
+        user_lower = list(),
+        user_upper = list(Vcmax = 400),
+        user_fit_options = list(Vcmax = 'FIT', alpha = 0, Rd = 'column')
+    )
+
+    expect_true('lower' %in% names(res))
+    expect_true('upper' %in% names(res))
+    expect_true('fit_options' %in% names(res))
+
+    expect_equal(names(res$lower), test_param_names)
+    expect_equal(names(res$upper), test_param_names)
+    expect_equal(names(res$fit_options), test_param_names)
+
+    expect_equal(res$lower$alpha, 0)
+    expect_equal(res$lower$Rd, 0)
+    expect_equal(res$lower$Vcmax, 0)
+    expect_equal(res$lower$TPU, 0)
+
+    expect_equal(res$upper$alpha, 1)
+    expect_equal(res$upper$Rd, 10)
+    expect_equal(res$upper$Vcmax, 400)
+    expect_equal(res$upper$TPU, 100)
+
+    expect_equal(res$fit_options$alpha, 0)
+    expect_equal(res$fit_options$Rd, 'column')
+    expect_equal(res$fit_options$Vcmax, 'fit')
+    expect_equal(res$fit_options$TPU, 40)
+})
+
+test_that('luf should be lists', {
+    expect_error(
+        PhotoGEA:::assemble_luf(
+            test_param_names,
+            default_lower = list(),
+            default_upper = list(),
+            default_fit_options = list(),
+            user_lower = list(alpha = 0, Rd = 0, Vcmax = 0),
+            user_upper = c(alpha = 1, Rd = 10, Vcmax = 1000),
+            user_fit_options = list(alpha = 'fit', Rd = 'column', Vcmax = 100)
+        ),
+        '`lower`, `upper`, and `fit_options` must be lists'
+    )
+})
+
+test_that('luf should be have names', {
+    expect_error(
+        PhotoGEA:::assemble_luf(
+            test_param_names,
+            default_lower = list(),
+            default_upper = list(),
+            default_fit_options = list(),
+            user_lower = list(alpha = 0, Rd = 0, Vcmax = 0),
+            user_upper = list(1, 10, 1000),
+            user_fit_options = list(alpha = 'fit', Rd = 'column', Vcmax = 100)
+        ),
+        '`lower`, `upper`, and `fit_options` must have named elements if they are not empty lists'
+    )
+})
+
+test_that('luf names cannot be empty', {
+    expect_error(
+        PhotoGEA:::assemble_luf(
+            test_param_names,
+            default_lower = list(),
+            default_upper = list(),
+            default_fit_options = list(),
+            user_lower = list(alpha = 0, Rd = 0, Vcmax = 0),
+            user_upper = list(alpha = 1, Rd = 10, Vcmax = 1000),
+            user_fit_options = list(alpha = 'fit', 'column', Vcmax = 100)
+        ),
+        '`lower`, `upper`, and `fit_options` must not have any empty names'
+    )
+})
+
+test_that('luf names must be in the parameter set', {
+    expect_error(
+        PhotoGEA:::assemble_luf(
+            test_param_names,
+            default_lower = list(),
+            default_upper = list(),
+            default_fit_options = list(),
+            user_lower = list(alpha = 0, Rd = 0, Vcmax = 0),
+            user_upper = list(alpha = 1, Rd = 10, Vcmax = 1000),
+            user_fit_options = list(alpha = 'fit', J = 'column', Vcmax = 100)
+        ),
+        '`lower`, `upper`, and `fit_options` must only provide settings for `alpha`, `Rd`, `Vcmax`, `TPU`'
+    )
+})
+
+
+test_that('fit options must be `fit`, `column`, or numeric', {
+    expect_error(
+        PhotoGEA:::assemble_luf(
+            test_param_names,
+            default_lower = list(),
+            default_upper = list(),
+            default_fit_options = list(),
+            user_lower = list(alpha = 0, Rd = 0, Vcmax = 0),
+            user_upper = list(alpha = 1, Rd = 10, Vcmax = 1000),
+            user_fit_options = list(alpha = 'WRONG VALUE', Rd = 'column', Vcmax = 100)
+        ),
+        'Each element of `fit_options` must be `fit`, `column`, or a numeric value'
+    )
+})
