ce38b6c79b4e7095a09b65b1ad58f1d8142aebf8
eloch216 <48919455+eloch216@users.noreply.github.com>
2025-01-08 21:42:23 -0500

    Relax `exdf` requirement

--- R/identify_c3_limiting_processes.R
+++ R/identify_c3_limiting_processes.R
@@ -1,5 +1,5 @@
 identify_c3_limiting_processes <- function(
-    exdf_obj,
+    data_table,
     a_column_name = 'A_fit',
     ac_column_name = 'Ac',
     aj_column_name = 'Aj',
@@ -7,10 +7,6 @@ identify_c3_limiting_processes <- function(
     tol = 1e-3
 )
 {
-    if (!is.exdf(exdf_obj)) {
-        stop('identify_c3_limits requires an exdf object')
-    }
-
     # Make sure the required variables are defined and have the correct units
     required_variables <- list()
     required_variables[[a_column_name]]  <- unit_dictionary[['A']]
@@ -18,45 +14,27 @@ identify_c3_limiting_processes <- function(
     required_variables[[aj_column_name]] <- unit_dictionary[['A']]
     required_variables[[ap_column_name]] <- unit_dictionary[['A']]
 
-    check_required_variables(exdf_obj, required_variables, check_NA = FALSE)
+    check_required_variables(data_table, required_variables, check_NA = FALSE)
 
     # Determine which process is limiting, allowing for the possibility of
     # co-limitation
-    exdf_obj[, 'Ac_limiting'] <- A_limiting(exdf_obj, a_column_name, ac_column_name, tol)
-    exdf_obj[, 'Aj_limiting'] <- A_limiting(exdf_obj, a_column_name, aj_column_name, tol)
-    exdf_obj[, 'Ap_limiting'] <- A_limiting(exdf_obj, a_column_name, ap_column_name, tol)
+    data_table[, 'Ac_limiting'] <- A_limiting(data_table, a_column_name, ac_column_name, tol)
+    data_table[, 'Aj_limiting'] <- A_limiting(data_table, a_column_name, aj_column_name, tol)
+    data_table[, 'Ap_limiting'] <- A_limiting(data_table, a_column_name, ap_column_name, tol)
 
-    exdf_obj[, 'limiting_process'] <- sapply(seq_len(nrow(exdf_obj)), function(i) {
-        limit_tuple <- as.numeric(
-            c(
-                exdf_obj[i, 'Ac_limiting'],
-                exdf_obj[i, 'Aj_limiting'],
-                exdf_obj[i, 'Ap_limiting']
-            )
-        )
+    data_table[, 'limiting_process'] <- 'unknown' # Should never occur
 
-        if (identical(limit_tuple, c(1, 0, 0))) {
-            'Ac'
-        } else if (identical(limit_tuple, c(0, 1, 0))) {
-            'Aj'
-        } else if (identical(limit_tuple, c(0, 0, 1))) {
-            'Ap'
-        } else if (identical(limit_tuple, c(1, 1, 0))) {
-            'co-limited (Ac and Aj)'
-        } else if (identical(limit_tuple, c(1, 0, 1))) {
-            'co-limited (Ac and Ap)'
-        } else if (identical(limit_tuple, c(0, 1, 1))) {
-            'co-limited (Aj and Ap)'
-        } else if (identical(limit_tuple, c(1, 1, 1))) {
-            'co-limited (Ac, Aj, and Ap)'
-        } else {
-            'unknown'
-        }
-    })
+    data_table[data_table[, 'Ac_limiting'] == TRUE  & data_table[, 'Aj_limiting'] == FALSE & data_table[, 'Ap_limiting'] == FALSE, 'limiting_process'] <- 'Ac'
+    data_table[data_table[, 'Ac_limiting'] == FALSE & data_table[, 'Aj_limiting'] == TRUE  & data_table[, 'Ap_limiting'] == FALSE, 'limiting_process'] <- 'Aj'
+    data_table[data_table[, 'Ac_limiting'] == FALSE & data_table[, 'Aj_limiting'] == FALSE & data_table[, 'Ap_limiting'] == TRUE,  'limiting_process'] <- 'Ap'
+    data_table[data_table[, 'Ac_limiting'] == TRUE  & data_table[, 'Aj_limiting'] == TRUE  & data_table[, 'Ap_limiting'] == FALSE, 'limiting_process'] <- 'co-limited (Ac and Aj)'
+    data_table[data_table[, 'Ac_limiting'] == TRUE  & data_table[, 'Aj_limiting'] == FALSE & data_table[, 'Ap_limiting'] == TRUE,  'limiting_process'] <- 'co-limited (Ac and Ap)'
+    data_table[data_table[, 'Ac_limiting'] == FALSE & data_table[, 'Aj_limiting'] == TRUE  & data_table[, 'Ap_limiting'] == TRUE,  'limiting_process'] <- 'co-limited (Aj and Ap)'
+    data_table[data_table[, 'Ac_limiting'] == TRUE  & data_table[, 'Aj_limiting'] == TRUE  & data_table[, 'Ap_limiting'] == TRUE,  'limiting_process'] <- 'co-limited (Ac, Aj, and Ap)'
 
     # Document and return
     document_variables(
-        exdf_obj,
+        data_table,
         c('identify_c3_limits', 'Ac_limiting',      ''),
         c('identify_c3_limits', 'Aj_limiting',      ''),
         c('identify_c3_limits', 'Ap_limiting',      ''),
