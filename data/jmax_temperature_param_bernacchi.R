ddf85a9037bf3dcb9633c66bd25ec475a5899a1e
eloch216 <48919455+eloch216@users.noreply.github.com>
2025-04-02 11:21:49 -0500

    Provide better defaults for calculating Jmax
    Now people won't need to change values at 25 when using different present options for temperature dependence

--- data/jmax_temperature_param_bernacchi.R
+++ data/jmax_temperature_param_bernacchi.R
@@ -1,4 +1,6 @@
 jmax_temperature_param_bernacchi <- list(
-    alpha_j_norm = list(type = 'Polynomial', coef = c(0.352, 0.022, -3.4e-4) / 0.6895,  units = 'normalized to alpha_j at 25 degrees C'),
-    theta_j_norm = list(type = 'Polynomial', coef = c(0.76,  0.018, -3.7e-4) / 0.97875, units = 'normalized to theta_j at 25 degrees C')
+    alpha_j_at_25 = list(type = 'Polynomial', coef = 0.2930375,                          units = 'dimensionless'),
+    alpha_j_norm  = list(type = 'Polynomial', coef = c(0.352, 0.022, -3.4e-4) / 0.6895,  units = 'normalized to alpha_j at 25 degrees C'),
+    theta_j_at_25 = list(type = 'Polynomial', coef = 0.97875,                            units = 'dimensionless'),
+    theta_j_norm  = list(type = 'Polynomial', coef = c(0.76,  0.018, -3.7e-4) / 0.97875, units = 'normalized to theta_j at 25 degrees C')
 )
--- data/jmax_temperature_param_flat.R
+++ data/jmax_temperature_param_flat.R
@@ -1,4 +1,6 @@
 jmax_temperature_param_flat <- list(
-    alpha_j_norm = list(type = 'Polynomial', coef = 1, units = 'normalized to alpha_j at 25 degrees C'),
-    theta_j_norm = list(type = 'Polynomial', coef = 1, units = 'normalized to theta_j at 25 degrees C')
+    alpha_j_at_25 = list(type = 'Polynomial', coef = 0.36125, units = 'dimensionless'),
+    alpha_j_norm  = list(type = 'Polynomial', coef = 1,       units = 'normalized to alpha_j at 25 degrees C'),
+    theta_j_at_25 = list(type = 'Polynomial', coef = 0.7,     units = 'dimensionless'),
+    theta_j_norm  = list(type = 'Polynomial', coef = 1,       units = 'normalized to theta_j at 25 degrees C')
 )
